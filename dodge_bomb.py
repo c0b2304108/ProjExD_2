6bf60f0f2f21fd09ab9d01efedb9cb0635e1e6c0
Darkelf7147 <c0b2304108@edu.teu.ac.jp>
2024-10-01 15:17:01 +0900

    Collosion judge

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -41,7 +41,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
+        if kk_rct.colliderect(bb_rct):
+            return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
