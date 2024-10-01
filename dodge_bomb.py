8bf5216744e3322b17d8cb11de64e04731e4c852
Darkelf7147 <c0b2304108@edu.teu.ac.jp>
2024-10-01 17:50:54 +0900

    shu

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -45,12 +45,7 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    # bb_img = pg.Surface((20,20))
-    # pg.draw.circle(bb_img,(255,0,0), (10,10),10)
-    # bb_img.set_colorkey((0,0,0))
-    # bb_rct = bb_img.get_rect()
-    # bb_rct.center = random.randint(0,WIDTH), random.randint(0,HEIGHT)
-    # vx, vy = +20, -20
+   
 
     bombs = [create_bomb()]
 
@@ -91,14 +86,6 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        # if key_lst[pg.K_UP]:
-        #     sum_mv[1] -= 5
-        # if key_lst[pg.K_DOWN]:
-        #     sum_mv[1] += 5
-        # if key_lst[pg.K_LEFT]:
-        #     sum_mv[0] -= 5
-        # if key_lst[pg.K_RIGHT]:
-        #     sum_mv[0] += 5
         for key, tpl in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += tpl[0]
@@ -110,12 +97,7 @@ def main():
 
         for i, (bb_img, bb_rct, vx, vy) in enumerate(bombs):
             bb_rct.move_ip(vx,vy)
-            #yoko, tate = check_bound(bb_rct)
-            
-            # if not yoko:
-            #     vx *= -1
-            # if not tate:
-            #     vy *= -1
+          
 
             if bb_rct.left < 0 or bb_rct.right > WIDTH:
                 vx *= -1
